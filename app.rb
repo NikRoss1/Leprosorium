9ed534b5187bde1b3b7f4f352699593340ad31bb
Nikolay Panov <nik.cmkw@yandex.ru>
2022-08-19 01:30:54 +0500

    Add details view

--- app.rb
+++ app.rb
@@ -72,5 +72,10 @@ end
 get '/details/:post_id' do
 	post_id = params[:post_id]
 
-	erb "Displaying information for post with id #{post_id}"
+	db = SQLite3::Database.new 'leprosorium.db'
+	db.results_as_hash = true
+	results = db.execute 'select * from Posts where id = ?',[post_id]
+	@row = results[0]
+
+	erb :details
 end
